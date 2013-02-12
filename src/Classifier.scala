1c002e5e231bfdf28b611978304e61bbfe3f5e48
unknown <schasins@gmail.com>
2013-02-12 11:55:00 -0800

    Moved file processing into its own function, general cleanup.

--- src/Classifier.scala
+++ src/Classifier.scala
@@ -22,44 +22,49 @@ object Classifier {
     return ls; 
   }
   
+  def fileToFrequencyDict(file : File) : mutable.Map[Int,Int] = {
+    var fileDict = mutable.Map.empty[Int,Int]; //maps words to frequency in curr file
+    var lines = scala.io.Source.fromFile(file).getLines();
+    for (line <- lines) {
+      for (word <- stringToTokens(line)) {
+        //if we haven't seen the word yet, add it to our dictionary
+        if (!Dict.dict.contains(word)) {
+          Dict.wordCount += 1;
+          Dict.dict += (word -> Dict.wordCount);
+        }
+        var index = Dict.dict(word);
+        //if we've seen this word in this file, increment in fileDict
+        if (fileDict.contains(index)){
+          fileDict(index)+=1;
+        }
+        //if we haven't seen this word in this file, add it to fileDict
+        else{
+          fileDict += (index -> 1);
+        }
+      }
+    }
+    return fileDict;
+  }
+  
   def filesToMatrices(files : Array[File]) : (BIDMat.SMat, BIDMat.SMat) = {
     var matrixEntries = mutable.ListBuffer.empty[(Int,Int,Int)]; //a record for each file-word pair, eventual matrix entries
     var fileCounter = 0;
     
     for (file <- files){
-        var fileDict = mutable.Map.empty[Int,Int]; //maps words to frequency in curr file
-        var lines = scala.io.Source.fromFile(file).getLines();
-	    for (line <- lines) {
-	      for (word <- stringToTokens(line)) {
-	        //if we haven't seen the word yet, add it to our dictionary
-	        if (!Dict.dict.contains(word)) {
-	          Dict.wordCount += 1;
-	          Dict.dict += (word -> Dict.wordCount);
-	        }
-	        var index = Dict.dict(word);
-	        //if we've seen this word in this file, increment in fileDict
-	        if (fileDict.contains(index)){
-	          fileDict(index)+=1;
-	        }
-	        //if we haven't seen this word in this file, add it to fileDict
-	        else{
-	          fileDict += (index -> 1);
-	        }
-	      }
-	    }
+        val fileDict = fileToFrequencyDict(file); //maps words to frequency in curr file
 	    //add the items from this file's fileDict to our matrix entries
 	    fileDict foreach {
-	      case (k, v) =>
-	        matrixEntries.append((k,fileCounter,v));
+	      case (wordIndex, frequency) =>
+	        matrixEntries.append((wordIndex,fileCounter,frequency));
 	    }
 	    fileCounter += 1;
     }
     
     val wordIndices = matrixEntries.map(x => x._1).toList;
     val fileIndices = matrixEntries.map(x => x._2).toList;
-    val frequencyValues = matrixEntries.map(x => x._3).toList;
+    val frequencyValues = matrixEntries.map(x => x._3).toList; //for freq matrix
     val len = wordIndices.length;
-    val presenceValues = List.fill(len){1};
+    val presenceValues = List.fill(len){1}; //for presence matrix
     
     val wordIndicesCol = icol(wordIndices);
     val fileIndicesCol = icol(fileIndices);
@@ -80,5 +85,10 @@ object Classifier {
     
     val posMatrices = filesToMatrices(posFiles);
     val negMatrices = filesToMatrices(negFiles);
+    
+    val posFrequencyMatrix = posMatrices._1;
+    val posPresenceMatrix = posMatrices._2;
+    val negFrequencyMatrix = negMatrices._1;
+    val negPresenceMatrix = negMatrices._2;
   }
 }
